cfdbc32feeb814da004b98690f4fa62b60864b46
Ravi Shankar <ravishankarsinghnissan56378@gmail.com>
2022-12-04 11:20:21 +0530

    add functionality in javascript

---
+++ script.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("matrix");
+console.log(canvas);
+const context = canvas.getContext("2d");
+
+// set height and width
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+// character for fall matrix
+// we can change it to any character
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const num = "0123456789";
+
+// joining all character
+// we can assign any value to "alphabet" variable, that we want to iritate
+const alphabet = latin + num;
+
+// assigning font-size and width of the columns
+const fontSize = 16;
+const columns = canvas.width / fontSize;
+
+const rainDrops = [];
+
+for (let x = 0; x < columns; x++) rainDrops[x] = 1;
+
+// creating draw function for filling and create matrix
+const draw = () => {
+  context.fillStyle = "rgba(0,0,0,0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = "#0F0";
+  context.font = fontSize + "px monospace";
+
+  for (let i = 0; i < rainDrops.length; i++) {
+    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+    if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975)
+      rainDrops[i] = 0;
+
+    rainDrops[i]++;
+  }
+};
+
+// calling draw() function on using 30 milisenconds interval
+setInterval(draw, 30);
